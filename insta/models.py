fe1cff66e38329d5319aaa1cdb16c42d4cd60e1c
stevekibe <stevekibe2345@gmail.com>
2018-10-09 10:57:21 +0300

    add the changes

--- insta/models.py
+++ insta/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 import datetime as dt
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from tinymce.models import HTMLField
+from django.dispatch import receiver
 
 
 
 class Profile(models.Model):
     title = models.CharField(max_length = 60)
-    Name = models.TextField()
+    caption = models.TextField()
     post = HTMLField()
     pub_date = models.DateTimeField(auto_now_add=True)
     profile_image = models.ImageField(upload_to = 'users/')
@@ -29,3 +31,21 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+
+class User(models.Model):
+    bio = HTMLField()
+    user_image = models.ImageField(upload_to ='users/')
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null="True")
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender,instance,created, **kwrgs):
+        if created:
+            User.objects.create(user=instance)
+
+    
