9bd01886e6e74d813bcd6859570e7f2efea88fa6
stevekibe <stevekibe2345@gmail.com>
2018-10-14 09:51:02 +0300

    add the changes in the code

--- insta/forms.py
+++ insta/forms.py
@@ -1,8 +1,26 @@
-from .models import Profile
+from .models import Image,Detail,Comment
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class NewPostForm(forms.ModelForm):
     class Meta:
-        model = Profile
-        exclude = ['pub_date']
-        
+        model = Image
+        exclude = ['pub_date','user', 'detail', 'likes']
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+class DetailForm(forms.ModelForm):
+    class Meta:
+        model = Detail
+        exclude = ['user']     
+
+class CommentsForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        fields = [ 'comment' ]
